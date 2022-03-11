457bf210799b04f6cc7eac65b240ad78f5b38921
Developer-Rakib <mdtazulislam6011@gmail.com>
2022-03-11 19:29:36 +0600

    bugfix

--- api.js
+++ api.js
@@ -1,54 +1,54 @@
-      
-        let Teams = document.getElementById("Teams");
-        let team = document.getElementById("team");
-        let inputFeild = document.getElementById("input-feild");
-        let load = document.getElementById("loading");
 
-        document.getElementById("search-btn").addEventListener("click", ()=>{
-            
-            if (isNaN(inputFeild.value) == false || inputFeild.value == "") {
-                alert("please enter valid input")
-            } else {
-                
-                load.style.display = "block";
-                Teams.textContent = "";
-                team.textContent = "";
-                let url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?s=${inputFeild.value}&c=Spain`
-                fetch(url)
-                .then(res => res.json())
-                .then(data => {
+let Teams = document.getElementById("Teams");
+let team = document.getElementById("team");
+let inputFeild = document.getElementById("input-feild");
+let load = document.getElementById("loading");
 
+document.getElementById("search-btn").addEventListener("click", () => {
 
-                    setTimeout(() => {
-                        displyData(data.teams);
+    if (isNaN(inputFeild.value) == false || inputFeild.value == "") {
+        alert("please enter valid input")
+    } else {
 
-                    }, 500);
-                    setTimeout(() => {
-
-                        load.style.display = "none";
-
-                    }, 500);
-                    
-                
-                    
-                })
-                inputFeild.value = "";
-            }
-        
-        
-    })
+        load.style.display = "block";
+        Teams.textContent = "";
+        team.textContent = "";
+        let url = `https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?s=${inputFeild.value}&c=Spain`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
 
 
-    function displyData (datas) {
-            if (datas == null) {
-                alert("Data Not Avaiable")
+                setTimeout(() => {
+                    displyData(data.teams);
 
-            } else {
-                datas.forEach(data => {
-                // console.log(data);
-                let div = document.createElement("div");
-                      
-                div.innerHTML = `
+                }, 500);
+                setTimeout(() => {
+
+                    load.style.display = "none";
+
+                }, 500);
+
+
+
+            })
+        inputFeild.value = "";
+    }
+
+
+})
+
+
+function displyData(datas) {
+    if (datas == null) {
+        alert("Data Not Avaiable")
+
+    } else {
+        datas.forEach(data => {
+            // console.log(data);
+            let div = document.createElement("div");
+
+            div.innerHTML = `
                             <img src="${data.strTeamBadge}" alt="">
                             <h5>Team Name</h5>
                             <h4>${data.strTeam}</h4>
@@ -56,15 +56,14 @@
                             <p>${data.strLeague}</p>
 
                 `;
-                Teams.appendChild(div);
-                
+            Teams.appendChild(div);
 
-                div.addEventListener("click", ()=> {
-                    team.textContent = "";
-                    let TeamDiv = document.createElement("div");
 
-                    console.log(data);
-                    TeamDiv.innerHTML = `
+            div.addEventListener("click", () => {
+                team.textContent = "";
+                location.hash = "#team";
+                let TeamDiv = document.createElement("div");
+                TeamDiv.innerHTML = `
                                 
                                 <img src="${data.strTeamBadge}" alt="">
                                 <h3> ${data.strTeam}</h3>
@@ -77,11 +76,11 @@
                                 <p>Description : ${data.strDescriptionEN.slice(0, 150)}</p>
 
                     `;
-                    team.appendChild(TeamDiv);
-                    Teams.setAttribute("for", "team");
+                team.appendChild(TeamDiv);
+                Teams.setAttribute("for", "team");
+                location.hash = "";
+            })
+        });
+    }
 
-                })
-            });
-            }
-           
-        }
+}
